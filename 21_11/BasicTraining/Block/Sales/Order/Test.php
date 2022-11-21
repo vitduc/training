e0f359a21ca283fe3d55167e2c46c9c273058d36
Ngô Viết Đức <87363075+vitduc@users.noreply.github.com>
2022-11-21 17:53:00 +0700

    Add files via upload

---
+++ 21_11/BasicTraining/Block/Sales/Order/Test.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Cowell\BasicTraining\Block\Sales\Order;
+
+use Magento\Sales\Model\Order;
+
+class Test extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * @var Order
+     */
+    protected $_order;
+    /**
+     * @var \Magento\Framework\DataObject
+     */
+    protected $_source;
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+    public function getSource()
+    {
+        return $this->_source;
+    }
+    public function displayFullSummary()
+    {
+        return true;
+    }
+    public function initTotals()
+    {
+        $parent = $this->getParentBlock();
+        $this->_order = $parent->getOrder();
+        $this->_source = $parent->getSource();
+        $title = 'Custom Total';
+        $store = $this->getStore();
+        if ($this->_order->getCustomTotal()!=0) {
+            $customAmount = new \Magento\Framework\DataObject(
+                [
+                    'code' => 'customfee',
+                    'strong' => false,
+                    'value' => $this->_order->getCustomTotal(),
+                    'label' => __($title),
+                ]
+            );
+            $parent->addTotal($customAmount, 'customfee');
+        }
+        return $this;
+    }
+    /**
+     * Get order store object
+     *
+     * @return \Magento\Store\Model\Store
+     */
+    public function getStore()
+    {
+        return $this->_order->getStore();
+    }
+    /**
+     * @return Order
+     */
+    public function getOrder()
+    {
+        return $this->_order;
+    }
+    /**
+     * @return array
+     */
+    public function getLabelProperties()
+    {
+        return $this->getParentBlock()->getLabelProperties();
+    }
+    /**
+     * @return array
+     */
+    public function getValueProperties()
+    {
+        return $this->getParentBlock()->getValueProperties();
+    }
+}
